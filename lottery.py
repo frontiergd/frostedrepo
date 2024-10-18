df38dcafee97b1fe6ba24d49004ee092df661e07
frontiergd <53576940+frontiergd@users.noreply.github.com>
2024-10-17 20:01:53 -0400

    lottery
    wow this is the lottery totally

---
+++ lottery.py
@@ -0,0 +1,13 @@
+import random
+
+lottonumbers1 = random.randint(1, 50)
+lottonumbers2 = random.randint(1,50)
+lottonumbers3 = random.randint(1, 50)
+lottonumbers4 = random.randint(1,50)
+lottonumbers5 = random.randint(1, 50)
+lottonumbers6 = random.randint(1,50)
+powerball = random.randint(1, 26)
+
+print("the lottery numbers are: " + str(lottonumbers1) + " " + str(lottonumbers2) + " " + str(lottonumbers3) + " " + str(lottonumbers4) + " " + str(lottonumbers5) + " " + str(lottonumbers6) + " powerball: " + str(powerball))
+
+print("goodbye")
